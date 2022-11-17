667dbd7556d208bd0b65aa1670c2bf7bcc0e3321
PLYER LOUIS <louis.plyer@unistra.fr>
2022-11-17 14:31:43 +0100

    prepare for publication

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_molsimilarity';
-$plugin->version   = 2022100700;
+$plugin->version   = 2022111700;
 $plugin->requires  = 2020060900;
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '1.2.1';
+$plugin->release = '1.2.2';
