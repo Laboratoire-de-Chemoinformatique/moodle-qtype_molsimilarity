74c9766d25097967d57acac72b7583db0e93e9a8
Céline Pervès <cperves@unistra.fr>
2025-02-21 14:15:18 +0100

    increment version and requires

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_molsimilarity';
-$plugin->version   = 2024061700;
-$plugin->requires  = 2022041904;
+$plugin->version   = 2025022100;
+$plugin->requires  = 2024100701;
 
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release = '1.4.3';
